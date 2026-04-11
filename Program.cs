2babdad2efa07712d139f930587636b956b50296
Gabriel dos Anjos <gabrieldosanjosdbz@gmail.com>
2026-04-10 21:00:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FiapHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fiap - Ola C#");
+            Console.Read();
+        }
+    }
+}
